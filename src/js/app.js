fdc63535c11caaf01bbc2daadc0021cacf6ff0cc
Angelica de Wit <angelicadewit@Angelicas-MacBook-Pro.local>
2018-07-24 22:22:14 -0400

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+var app = new Vue({
+	el: "#app",
+	data: {
+		newName:``,
+		newEmail:``,
+		people: [],
+	},
+	methods:{
+		addPerson: function(){
+			let newPerson =  {
+				image: `https://www.gravatar.com/avatar/` + md5(this.newEmail),
+				name: this.newName,
+				email: this.newEmail,
+				timestamp: new Date().getTime(),
+			}
+			this.people.push(newPerson)
+			this.newName = ``
+			this.newEmail = ``
+			if (this.people.length > 2){
+				this.people.splice(this.index, 1)
+			}
+		}
+	}
+})
